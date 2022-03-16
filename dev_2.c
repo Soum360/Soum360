4a800c084e555b3239ffdfe90d270eda2ff506cc
Soum360 <101164733+Soum360@users.noreply.github.com>
2022-03-16 10:59:21 +0100

    Update dev_2.c

--- dev_2.c
+++ dev_2.c
@@ -1,21 +1,21 @@
-# inclure < stdio.h >
-# inclure < unistd.h >
-# inclure < sys/wait.h >
-# inclure < errno.h >
-int main ( int argc, char *argv[]){ 
-int je ; pid_t pid[ 4 ] ; 
-pour (je = 0 ; je < 4 ; je++){
- pid[i] = fourchette (); 
-si (pid[i]< 0 ){ /* On gère l'erreur */
- fprintf (stderr, " Erreur lors de la création du fils( %d ) \n " , errno); 
-retour 1 ; } 
-si (pid[i] == 0 ){ /* Dans le fils */ 
-fprintf (stdout, " Je suis ( %d ), mon père ( %d ) \n " , getpid (), getppid ()); 
-casser ; 
-} sinon { } 
-} 
-pour (je = 0 ; je < 4 ; je++){ 
-état entier ; 
-attendre (&statut); 
-}
-}
+#include<unistd.h>
+//#include<sys/wait.h>
+#include<errno.h>
+int  main ( int argc, char *argv[]){
+  int i ;
+  pid_t pid[ 4 ] ;
+  for (i = 0 ; i < 4 ; i++)
+  {
+    pid[i] = fork();
+    if (pid[i]< 0 )
+        {
+      /* On gère l'erreur */
+      fprintf (stderr, " Erreur lors de la création du fils( %d ) \n " , errno);
+      return  1 ;
+    }
+    if (pid[i] == 0 )
+    {
+      /* Dans le fils */
+      fprintf (stdout, " Je suis ( %d ), mon père ( %d ) \n " , getpid (), getppid ());
+      break ;
+    }
